221d714a2f20c30ac6ef971f483001dd3d83f278
Dave Burke <daveburkevt@gmail.com>
2017-07-11 15:48:41 -0400

    old comment removal

--- src/main/java/com/nixmash/mvc/views/PageView.java
+++ src/main/java/com/nixmash/mvc/views/PageView.java
@@ -2,8 +2,6 @@ package com.nixmash.mvc.views;
 
 import io.bootique.mvc.AbstractView;
 
-// intentionally keeping the view class in a different package
-// from API and IT to test per-package template laoding
 public class PageView extends AbstractView {
 
     private Object model;
